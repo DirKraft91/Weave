2afdac2a924c62fc94e81a94c64eb1dbb729d353
Ruslan Shlinchak <ruslanshlinchak@gmail.com>
2025-02-10 19:20:19 +0200

    fix: structure

--- server/src/main.rs
+++ server/src/main.rs
@@ -1,35 +1,13 @@
 mod operations;
-mod router;
 mod proof;
 mod services;
 
-use anyhow::{anyhow, Context, Result};
-use keystore_rs::{KeyChain, KeyStore};
-use log::{debug, info};
-use prism_da::{memory::InMemoryDataAvailabilityLayer, DataAvailabilityLayer};
-use prism_keys::SigningKey;
-use prism_storage::inmemory::InMemoryDatabase;
-use std::sync::Arc;
+use anyhow::Result;
 use tokio::spawn;
-use prism_prover::{webserver::WebServerConfig, Config, Prover};
-use crate::operations::register_service;
-use crate::router::create_router;
+use crate::services::api::ApiService;
+use crate::services::node::NodeService;
 
 pub static SERVICE_ID: &str = "weave_service";
-
-
-async fn start_api_server() {
-    let app = create_router();
-    let listen_addr = "0.0.0.0:8080".to_string();
-    info!("webserver listening on {}", listen_addr);
-    println!("webserver listening on {}", listen_addr);
-    if let Err(e) = axum::Server::bind(&listen_addr.parse().unwrap())
-        .serve(app.into_make_service())
-        .await
-        .context("Failed to start server") {
-            log::error!("Server error: {}", e);
-    }
-}
 
 #[tokio::main]
 async fn main() -> Result<()> {
@@ -38,48 +16,23 @@ async fn main() -> Result<()> {
             "DEBUG,ctclient::internal=off,reqwest=off,hyper=off,tracing=off,sp1_stark=info,jmt=off,p3_dft=off,p3_fri=off,sp1_core_executor=info,sp1_recursion_program=info,p3_merkle_tree=off,sp1_recursion_compiler=off,sp1_core_machine=off",
         );
     pretty_env_logger::init();
-    let api_server_handle = spawn(start_api_server());
-    let db = InMemoryDatabase::new();
-    let (da_layer, _, _) = InMemoryDataAvailabilityLayer::new(5);
-    let keystore_sk = KeyChain.get_or_create_signing_key(SERVICE_ID)?;
-    let sk = SigningKey::Ed25519(Box::new(keystore_sk.clone()));
-    let cfg = Config {
-        prover: true,
-        batcher: true,
-        webserver: WebServerConfig {
-            enabled: true,
-            host: "127.0.0.1".to_string(),
-            port: 50524,
-        },
-        signing_key: sk.clone(),
-        verifying_key: sk.verifying_key(),
-        start_height: 1,
-    };
 
-    let prover = Arc::new(
-        Prover::new(
-            Arc::new(Box::new(db)),
-            Arc::new(da_layer) as Arc<dyn DataAvailabilityLayer>,
-            &cfg,
-        )
-        .unwrap(),
-    );
-
-    register_service(prover.clone()).await?;
-
-    let runner = prover.clone();
-    let runner_handle = spawn(async move {
-        debug!("starting prover");
-        if let Err(e) = runner.run().await {
-            log::error!("Error occurred while running prover: {:?}", e);
-        }
-    });
+    let node_service = NodeService::new();
+    let api_service = ApiService::new(&node_service.prover);
 
     tokio::select! {
-        _ = runner_handle => {
+        _ = spawn(async move {
+            if let Err(e) = node_service.run_prover().await {
+                log::error!("Error occurred while running prover: {:?}", e);
+            }
+        }) => {
             println!("Prover runner task completed");
         }
-        _ = api_server_handle => {
+        _ = spawn(async move {
+            if let Err(e) = api_service.start_server().await {
+                log::error!("Error occurred while running API server: {:?}", e);
+            }
+        }) => {
             println!("API server task completed");
         }
     }
---
+++ server/src/services/api.rs
@@ -0,0 +1,67 @@
+use anyhow::Context;
+use log::info;
+use anyhow::Result;
+use prism_prover::Prover;
+use std::sync::Arc;
+
+use axum::{
+    routing::post,
+    Router,
+    middleware,
+};
+use tower_http::cors::{CorsLayer, Any};
+use http::{
+    header::HeaderName,
+    method::Method
+};
+use crate::proof::apply_proof;
+use crate::services::auth::{auth, auth_middleware};
+
+pub struct ApiService {
+    prover: Arc<Prover>,
+}
+
+impl ApiService {
+    pub fn new(prover: &Arc<Prover>) -> Self {
+        Self { prover: prover.clone() }
+    }
+
+    pub async fn start_server(&self) -> Result<()> {
+        let app = self.create_router();
+        let listen_addr = "0.0.0.0:8080".to_string();
+        info!("webserver listening on {}", listen_addr);
+        if let Err(e) = axum::Server::bind(&listen_addr.parse().unwrap())
+            .serve(app.into_make_service())
+            .await
+            .context("Failed to start server") {
+                log::error!("Server error: {}", e);
+        }
+        Ok(())
+    }
+
+    fn create_router(&self) -> Router {
+        let cors = CorsLayer::new()
+            .allow_origin(Any)
+            .allow_methods(vec![Method::GET, Method::POST, Method::PUT, Method::DELETE])
+            .allow_headers(vec![
+                HeaderName::from_static("content-type"),
+                HeaderName::from_static("authorization"),
+            ]);
+    
+        // Public routes that don't require authentication
+        let public_routes = Router::new()
+            .route("/auth", post(auth));
+    
+        // Protected routes that require authentication
+        let protected_routes = Router::new()
+            .route("/proof", post(apply_proof))
+            .layer(middleware::from_fn(auth_middleware));
+    
+        // Combine the routes
+        Router::new()
+            .merge(public_routes)
+            .merge(protected_routes)
+            .layer(cors)
+            .with_state(self.prover.clone())
+    }
+}
