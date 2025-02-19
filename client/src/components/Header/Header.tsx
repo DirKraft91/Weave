c754eafac66867c3057a2fd29192ed6d07ed94a3
Ruslan Shlinchak <ruslanshlinchak@gmail.com>
2025-02-19 14:35:08 +0200

    fix: header feat structure

--- client/src/components/Header/Header.tsx
+++ client/src/components/Header/Header.tsx
@@ -1,13 +1,11 @@
-import { Chip, Button } from '@heroui/react';
-import { ArrowLeftStartOnRectangleIcon } from '@heroicons/react/24/outline';
+import { Auth } from './components/Auth';
+import { WalletConnect } from './components/Wallet';
 
 export const Header = () => {
   return (
     <header className="flex gap-4">
-      <Chip color="primary" startContent={<ArrowLeftStartOnRectangleIcon width={24} height={24} />} variant="faded">
-        Wallet
-      </Chip>
-      <Button color="primary">Button</Button>
+      <Auth />
+      <WalletConnect />
     </header>
   );
 };
