2b1b0cb0e7a0cffe90ed0d449d47f1bf99bc8912
Ruslan Shlinchak <ruslanshlinchak@gmail.com>
2025-02-23 19:24:06 +0200

    fix: dashboard auth hotfix

--- client/src/routes/_unathenticated/index.tsx
+++ client/src/routes/_unathenticated/index.tsx
@@ -1,8 +1,8 @@
 import { AuthModal } from '@/components/AuthModal/AuthModal';
 import { WalletConnectModal } from '@/components/WalletConnectModal/WalletConnectModal';
-import { useWalletStore } from '@/contexts';
+import { useAuthStore, useWalletStore } from '@/contexts';
 import { useDisclosure, Button } from '@heroui/react';
-import { createFileRoute } from '@tanstack/react-router';
+import { createFileRoute, useNavigate } from '@tanstack/react-router';
 import { WalletStatus } from '@cosmos-kit/core';
 
 export const Route = createFileRoute('/_unathenticated/')({
@@ -13,10 +13,17 @@ function RouteComponent() {
   const authModal = useDisclosure();
   const walletConnectModal = useDisclosure();
   const { selectedWallet } = useWalletStore();
+  const { authToken } = useAuthStore();
+  const navigate = useNavigate();
 
+  const isAuthenticated = !!authToken;
   const isWalletConnected = selectedWallet?.walletStatus === WalletStatus.Connected;
 
   const handleClick = () => {
+    if (isAuthenticated) {
+      navigate({ to: '/dashboard' });
+    }
+
     if (isWalletConnected) {
       authModal.onOpen();
     } else {
@@ -33,7 +40,7 @@ function RouteComponent() {
           Secure your identity.
         </h1>
         <Button variant="solid" color="secondary" size="lg" onClick={handleClick}>
-          {isWalletConnected ? 'Start Now' : 'Connect Wallet'}
+          {isAuthenticated ? 'Go To Dashboard' : isWalletConnected ? 'Start Now' : 'Connect Wallet'}
         </Button>
         <AuthModal {...authModal} />
         <WalletConnectModal {...walletConnectModal} />
