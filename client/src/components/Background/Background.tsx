b15e28fb12357ec43d3c9f1cb16cc135408eed4e
DmitriyTymoshchenko <dtforweb@gmail.com>
2025-02-25 20:42:35 +0100

    feat: add gradient background image to Background component

--- client/src/components/Background/Background.tsx
+++ client/src/components/Background/Background.tsx
@@ -1,5 +1,9 @@
+import GradientImage from '@/assets/gradient.svg';
+
 export function Background() {
   return (
-    <div className="z-0 absolute inset-0 h-full w-full bg-[radial-gradient(rgba(255,255,255,0.8),transparent_1px)] [background-size:16px_16px] [mask-image:radial-gradient(ellipse_60%_60%_at_50%_50%,#000_60%,transparent_100%)]" />
+    <div className="z-0 absolute inset-0 h-full w-full bg-[radial-gradient(rgba(255,255,255,0.8),transparent_1px)] [background-size:16px_16px] [mask-image:radial-gradient(ellipse_60%_60%_at_50%_50%,#000_60%,transparent_100%)]">
+      <img src={GradientImage} alt="Gradient" className="w-full h-full object-cover" />
+    </div>
   );
 }
