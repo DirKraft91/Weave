43b7f26d0276ce70a379367c6ee01576b8f3fa09
DmitriyTymoshchenko <dtforweb@gmail.com>
2025-02-25 17:49:11 +0100

    feat: add user count

--- client/src/components/Footer/Footer.tsx
+++ client/src/components/Footer/Footer.tsx
@@ -1,5 +1,5 @@
 import SpiderInteresting from '@/assets/spider-interesting.png';
-import { FaGithub, FaXTwitter } from 'react-icons/fa6';
+import { FaGithub, FaUser, FaXTwitter } from 'react-icons/fa6';
 
 export const Footer = () => {
   const currentYear = new Date().getFullYear();
@@ -18,6 +18,11 @@ export const Footer = () => {
       <div className="flex items-center gap-4">
         <p className="text-sm text-white">© {currentYear} Weave</p>
         <img src={SpiderInteresting} alt="Spider Interesting" className="w-8" />
+      </div>
+
+      <div className="flex items-center gap-2 ml-6">
+        <FaUser size={18} className="text-white" />
+        <span className="text-sm rounded-full bg-white/10 w-6 h-6 flex items-center justify-center text-white text-small">5</span>
       </div>
     </footer>
   );
